844f1bfb347d012953f5266fbe5fe0e85a19ea72
Gibson <95596261+Gibsonmb71@users.noreply.github.com>
2023-11-18 18:18:46 -0500

    Create snowfall.js

---
+++ snowfall.js
@@ -0,0 +1,18 @@
+// snowfall.js
+document.addEventListener('DOMContentLoaded', function() {
+    let container = document.querySelector('.container');
+
+    for (let i = 0; i < 30; i++) {
+        let snowflake = document.createElement('div');
+        snowflake.className = 'snowflake';
+        container.appendChild(snowflake);
+
+        let size = Math.random() * 3;
+        snowflake.style.width = size + 'px';
+        snowflake.style.height = size + 'px';
+        snowflake.style.left = Math.random() * window.innerWidth + 'px';
+        snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+
+        container.appendChild(snowflake);
+    }
+});
